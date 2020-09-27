4dccfb47b6c10c8c10b9377edd1f7a8d3e0adf01
ryota-sb <ryota.sb.1206@gmail.com>
2020-09-27 14:26:56 +0900

    vuetify追加 layout変更

--- src/main.js
+++ src/main.js
@@ -3,11 +3,28 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+import aws_exports from './aws-exports'
+import { components } from 'aws-amplify-vue'
 
 Vue.config.productionTip = false
+
+Amplify.configure(aws_exports)
+
+Vue.use(AmplifyPlugin, AmplifyModules)
 
 new Vue({
   router,
   store,
+
+  comoponents: {
+    App,
+    ...components
+  },
+
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
